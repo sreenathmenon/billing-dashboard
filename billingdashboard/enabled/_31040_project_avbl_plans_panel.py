59a1628142cf4d667c361bee7e2a6c13f129ba02
Sreenath <sreenath.mm@poornam.com>
2017-10-10 23:21:42 +0530

    Adding the enabled files

---
+++ billingdashboard/enabled/_31040_project_avbl_plans_panel.py
@@ -0,0 +1,6 @@
+PANEL_GROUP = 'billing'
+PANEL_DASHBOARD = 'project'
+PANEL = 'customer_available_plans'
+
+ADD_PANEL = \
+    'billingdashboard.dashboards.project.available_plans.panel.Project_AvailablePlans'
