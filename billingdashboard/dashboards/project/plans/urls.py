03414cbb1a4e422283a4dd44891e44f91e15e19f
Sreenath <sreenath.mm@poornam.com>
2017-10-10 23:35:06 +0530

    Removing unwanted files

--- billingdashboard/dashboards/project/plans/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns
-from django.conf.urls import url
-
-from billingdashboard.dashboards.project.plans import views
-
-urlpatterns = patterns(
-    '',
-    url(r'^$', views.IndexView.as_view(), name='index'),
-)
--- billingdashboard/dashboards/project/plans/views.py
+++
@@ -1,7 +0,0 @@
-from django.views import generic
-
-
-class IndexView(generic.TemplateView):
-    template_name = 'project/plans/index.html'
-
-
